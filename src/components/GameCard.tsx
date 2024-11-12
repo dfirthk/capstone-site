38fbdf73770d68a67410dbde513164fe4807b66b
dfirthk <dfirthk@wgu.edu>
2024-11-12 07:53:13 -0800

    Adding existing files to new repo

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,28 @@
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
+import { Game } from '../hooks/useGames';
+import getCroppedImageURL from '../services/image-url';
+import CriticScore from './CriticScore';
+import PlatformIconList from './PlatformIconList';
+
+interface Props {
+	game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+	return (
+		<Card>
+			<Image src={getCroppedImageURL(game.background_image)} />
+			<CardBody>
+				<Heading fontSize="2xl">{game.name}</Heading>
+				<HStack justifyContent="space-between">
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					/>
+					<CriticScore score={game.metacritic} />
+				</HStack>
+			</CardBody>
+		</Card>
+	);
+};
+
+export default GameCard;
---
+++ src/components/GameGrid.tsx
@@ -0,0 +1,40 @@
+import { SimpleGrid, Text } from '@chakra-ui/react';
+import { GameQuery } from '../App';
+import useGames, { Platform } from '../hooks/useGames';
+import { Genre } from '../hooks/useGenres';
+import GameCard from './GameCard';
+import GameCardContainer from './GameCardContainer';
+import GameCardSkeleton from './GameCardSkeleton';
+
+interface Props {
+	gameQuery: GameQuery;
+}
+
+const GameGrid = ({ gameQuery }: Props) => {
+	const { data, error, isLoading } = useGames(gameQuery);
+	const skeletons = [1, 2, 3, 4, 5, 6];
+	return (
+		<>
+			{error && <Text>{error}</Text>}
+			<SimpleGrid
+				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+				padding="10"
+				spacing={3}
+			>
+				{isLoading &&
+					skeletons.map((skeleton) => (
+						<GameCardContainer key={skeleton}>
+							<GameCardSkeleton />
+						</GameCardContainer>
+					))}
+				{data.map((data) => (
+					<GameCardContainer key={data.id}>
+						<GameCard game={data} />
+					</GameCardContainer>
+				))}
+			</SimpleGrid>
+		</>
+	);
+};
+
+export default GameGrid;

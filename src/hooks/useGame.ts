43b9bc025aeaae58cbc0a60345fa282d49927b40
dfirthk <dfirthk@wgu.edu>
2024-11-21 14:00:03 -0800

    Refactor - move entity interfaces

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import { Game } from "../entities/Game";
 import APICleint from "../services/api-client";
-import { Game } from "./useGames";
 
 const apiClient = new APICleint<Game>('/games');
 

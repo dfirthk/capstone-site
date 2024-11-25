cb3d4ca0aa71ed6dd2d39853c3e89d18fe517ab7
dfirthk <dfirthk@wgu.edu>
2024-11-25 14:11:22 -0800

    reverted main

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,34 +1,19 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-import ms from "ms";
-import platforms from "../data/platforms";
-import Platform from "../entities/Platform";
-import APIClient, { FetchResponse } from "../services/api-client";
-import useGameQueryStore from "../store";
+import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
+import platforms from '../data/platforms';
+import Platform from '../entities/Platform';
+import APIClient from '../services/api-client';
 
-const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+const apiClient = new APIClient<Platform>(
+  '/platforms/lists/parents'
+);
 
-const usePlatforms = () => {
-	const gameQuery = useGameQueryStore(s => s.gameQuery);
-	
-	return useInfiniteQuery<FetchResponse<Platform>, Error>({
-		queryKey: ['parent_platforms', gameQuery],
-		queryFn: ({pageParam = 1}) =>
-			apiClient
-				.getAll({
-					params: {
-						parent_platforms: gameQuery.platformId,
-						ordering: gameQuery.sortOrder,
-						search: gameQuery.searchText,
-						page: pageParam
-						},
-				}),
-				getNextPageParam: (lastPage, allPages) => {
-					return lastPage.next ? allPages.length + 1 : undefined;
-				},
-				staleTime: ms('24h')
-	})
-}
-	
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ['platforms'],
+    queryFn: apiClient.getAll,
+    staleTime: ms('24h'),
+    initialData: platforms,
+  });
+
 export default usePlatforms;
-
-
--- src/hooks/usePlatorm.ts
+++ src/hooks/usePlatorm.ts
@@ -1,17 +1,8 @@
-import { useQuery } from "@tanstack/react-query";
-import ms from "ms";
-import platforms from "../data/platforms";
-import Platform from "../entities/Platform";
-import APIClient from "../services/api-client";
+import usePlatforms from "./usePlatforms";
 
-const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+const usePlatform = (id?: number) => {
+   const { data: platforms } = usePlatforms();
+	return platforms?.results.find((p) => p.id === id);
+}
 
-const usePlatforms = () => useQuery({
-   queryKey: ['platforms'],
-   queryFn: apiClient.getAll,
-   staleTime: ms('24h'),
-   initialData: platforms
-
-})
-
-export default usePlatforms;
+export default usePlatform;

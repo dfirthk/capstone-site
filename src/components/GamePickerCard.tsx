57dd6d619a8b25cb32245d7955113327bb9fbbb1
dfirthk <dfirthk@wgu.edu>
2024-11-27 16:26:54 -0800

    useSelection implemented to Genre and Game

--- src/components/GamePickerCard.tsx
+++ src/components/GamePickerCard.tsx
@@ -1,6 +1,6 @@
-import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Box, Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import Game from '../entities/Game';
+import { useSelection } from '../hooks/cardSelection';
 import getCroppedImageURL from '../services/image-url';
 import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
@@ -10,19 +10,36 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
+	const { selectedItems, toggleSelection } = useSelection();
+
+	const isSelected = selectedItems.some(
+		(item) => item.type === 'game' && item.value === game.name
+	);
+
 	return (
-		<Card>
-			<Image src={getCroppedImageURL(game.background_image)} />
-			<CardBody>
-				<HStack justifyContent="space-between" marginBottom={3}>
-					<PlatformIconList
-						platforms={game.parent_platforms.map((p) => p.platform)}
-					/>
-					<CriticScore score={game.metacritic} />
-				</HStack>
-				<Heading fontSize="2xl">{game.name}</Heading>
-			</CardBody>
-		</Card>
+		<Box
+			as="button"
+			p={0}
+			borderWidth="10px"
+			borderColor={isSelected ? 'green.500' : 'gray.200'}
+			borderRadius="lg"
+			_hover={{ borderColor: 'green.400' }}
+			onClick={() => toggleSelection('game', game.name)}
+			transition="border-color 0.2s"
+		>
+			<Card>
+				<Image src={getCroppedImageURL(game.background_image)} />
+				<CardBody>
+					<HStack justifyContent="space-between" marginBottom={3}>
+						<PlatformIconList
+							platforms={game.parent_platforms.map((p) => p.platform)}
+						/>
+						<CriticScore score={game.metacritic} />
+					</HStack>
+					<Heading fontSize="2xl">{game.name}</Heading>
+				</CardBody>
+			</Card>
+		</Box>
 	);
 };
 
--- src/components/GenreCard.tsx
+++ src/components/GenreCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react';
+import { Box, Card, CardBody, Heading, Image } from '@chakra-ui/react';
 import Genre from '../entities/Genre';
+import { useSelection } from '../hooks/cardSelection';
 import getCroppedImageURL from '../services/image-url';
 
 interface Props {
@@ -7,13 +8,29 @@ interface Props {
 }
 
 const GenreCard = ({ genre }: Props) => {
+	const { selectedItems, toggleSelection } = useSelection();
+
+	const isSelected = selectedItems.some(
+		(item) => item.type === 'genre' && item.value === genre.name
+	);
 	return (
-		<Card>
-			<Image src={getCroppedImageURL(genre.image_background)} />
-			<CardBody>
-				<Heading fontSize="2xl">{genre.name}</Heading>
-			</CardBody>
-		</Card>
+		<Box
+			as="button"
+			p={0}
+			borderWidth="10px"
+			borderColor={isSelected ? 'green.500' : 'gray.200'}
+			borderRadius="lg"
+			_hover={{ borderColor: 'green.400' }}
+			onClick={() => toggleSelection('genre', genre.name)}
+			transition="border-color 0.2s"
+		>
+			<Card>
+				<Image src={getCroppedImageURL(genre.image_background)} />
+				<CardBody>
+					<Heading fontSize="2xl">{genre.name}</Heading>
+				</CardBody>
+			</Card>
+		</Box>
 	);
 };
 
